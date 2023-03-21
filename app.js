aaf4c4e2b900d6a47128e0509cbdb71b8935a54a
thelaurafranklin <thelaurafranklin@gmail.com>
2023-03-20 17:59:17 -0700

    add mongoose connection

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Mongoose setup
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const URI = process.env.MONGO_URI;
+
+main().catch((err) => console.error(err));
+async function main() {
+  await mongoose.connect(URI);
+}
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
