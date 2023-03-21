34a70f9e61d9a9368e1758273def1642848c2a46
thelaurafranklin <thelaurafranklin@gmail.com>
2023-03-20 17:41:36 -0700

    set up basic app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
